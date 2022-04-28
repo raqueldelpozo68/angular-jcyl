ea78441322ec2a57b35f12b312e689d34092650f
pozgarra <raquel.delpozo@jcyl.es>
2022-04-28 12:31:12 +0200

    Ejercicio 6_1

--- src/app/solicitud/solicitud.component.ts
+++ src/app/solicitud/solicitud.component.ts
@@ -1,19 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-solicitud',
   templateUrl: './solicitud.component.html',
-  styleUrls: ['./solicitud.component.css']
+  styleUrls: ['./solicitud.component.css'],
+
 })
 export class SolicitudComponent implements OnInit {
-  
+
+  solicitud = {nombre: "", apellidos:""}
+  solicitudComedor = 'Solicitud de Comedor';
+  nuevaSolicitud = 'Nueva Solicitud';
+
   constructor() { 
-}
+  }
+  
   ngOnInit(): void {
   }
 
-  hi(){
-    console.log('Hi');
+  enviar(solicitud:any){
+    console.log('Enviada', solicitud.nombre, solicitud.apellidos);
   }
+  actualizarApellidos($event: KeyboardEvent){
+      const element = $event.target as HTMLInputElement;
+      this.solicitud.apellidos = element.value; 
+      console.log('Actualizados Apellidos: ', this.solicitud.apellidos);
+      }
+  actualizarNombre($event: KeyboardEvent){
+    const element = $event.target as HTMLInputElement;
+    this.solicitud.nombre = element.value; 
+    console.log('Actualizado Nombre: ', this.solicitud.nombre);
+    }
+  } 
 
-}
+
