72db5a2c2fbf05509a6d0867a33f2480aa70b2ca
pozgarra <raquel.delpozo@jcyl.es>
2022-05-09 10:48:06 +0200

    Ejercicio 10

--- src/app/centro/centro.component.spec.ts
+++ src/app/centro/centro.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CentroComponent } from './centro.component';
 
 describe('CentroComponent', () => {
--- src/app/centro/centro.component.ts
+++ src/app/centro/centro.component.ts
@@ -1,27 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CentrosComponent, tipoCentro } from '../centros/centros.component';
+
+
 
 @Component({
   selector: 'app-centro',
-  //templateUrl: './centro.component.html',
-  template:`
-       <h1>Centro</h1>
-               <div class="mb-3 mt-3">
-                   <label for="nombre" class="form-label">Nombre:</label>
-                   {{centro.nombre}}
-               </div>                    
-      
-               <div class="mb-3 mt-3">
-                 <label for="codigo" class="form-label">Codigo:</label>
-                    {{centro.codigo}}
-               </div>
-  `,
+  templateUrl: './centro.component.html',
   styleUrls: ['./centro.component.css']
 })
+
 export class CentroComponent implements OnInit {
-  centro = {nombre: "IES Lo imposible", codigo:"003"};
+
+ @Input() centro?:tipoCentro;
   constructor() { }
 
   ngOnInit(): void {
   }
 
-}
+
+}	
